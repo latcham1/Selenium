9077fcebd81d0f51e45958e9566b08582d07c391
latcham1 <latcham.appasamy@gmail.com>
2023-06-11 04:26:18 -0400

    unstage files commit

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -2,7 +2,7 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-@CucumberOptions(features="src/test/java/features/login.feature",
+@CucumberOptions(features="src/test/java/features/Login.feature",
                          glue="steps",
                          monochrome=true,
                          publish=true)
